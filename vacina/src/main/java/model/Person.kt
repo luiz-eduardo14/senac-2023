407b54dbca3ed15f6677f371dbb75e3f44726b52
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-03-20 18:14:29 -0300

    feat: change type of prop dateOfBirth for LocalDate

--- vacina/src/main/java/model/Person.kt
+++ vacina/src/main/java/model/Person.kt
@@ -1,6 +1,6 @@
 package model
 
-import java.sql.Date
+import java.time.LocalDate
 
 enum class Sex(s: String) {
     MALE("M"),
@@ -11,11 +11,11 @@ class Person {
     var name: String? = "";
     var age: Short? = 0;
     var sex: Sex? = null;
-    var dateOfBirth: Date? = null;
+    var dateOfBirth: LocalDate? = null;
     var cpf: String? = null;
     var id: Int? = null;
 
-    constructor(name: String, age: Short, sex: Sex?, dateOfBirth: Date?, cpf: String?, id: Int?) {
+    constructor(name: String, age: Short, sex: Sex?, dateOfBirth: LocalDate?, cpf: String?, id: Int?) {
         this.name = name
         this.age = age
         this.sex = sex
