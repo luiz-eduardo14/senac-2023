0184a3d79898b24f36ae14633ef375e7073b8855
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-03-21 00:24:45 -0300

    feat: update class Person for use with jpa

--- vacina/src/main/java/model/Person.kt
+++ vacina/src/main/java/model/Person.kt
@@ -1,21 +1,32 @@
 package model
 
 import java.time.LocalDate
+import javax.persistence.*
 
 enum class Sex(s: String) {
     MALE("M"),
     FEMALE("F")
 }
 
+@Entity
+@Table(name = "person")
 class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+    @Column(name = "name")
     var name: String? = "";
-    var age: Short? = 0;
+    @Column(name = "age")
+    var age: Int? = 0;
+    @Column(name = "sex")
+    @Enumerated(EnumType.STRING)
     var sex: Sex? = null;
+    @Column(name = "dateOfBirth")
     var dateOfBirth: LocalDate? = null;
+    @Column(name = "cpf")
     var cpf: String? = null;
-    var id: Int? = null;
 
-    constructor(name: String, age: Short, sex: Sex?, dateOfBirth: LocalDate?, cpf: String?, id: Int?) {
+    constructor(name: String, age: Int, sex: Sex?, dateOfBirth: LocalDate?, cpf: String?, id: Long?) {
         this.name = name
         this.age = age
         this.sex = sex
