678927e5b6f346a8a17f703f295e9b9fea869aa1
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-03-21 00:23:43 -0300

    feat: modify class of database for generate entityManagerFactory

---
+++ vacina/src/main/java/config/Database.kt
@@ -0,0 +1,30 @@
+package config;
+
+import org.hibernate.cfg.Environment
+import java.util.Optional
+import java.util.logging.Level
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
+
+class Database {
+    companion object{
+        private fun createEntityManager(listEnv: Map<String,String>): EntityManagerFactory{
+            val mapConfigHibernete = HashMap<String,String>();
+            mapConfigHibernete[Environment.URL] = Optional.ofNullable(listEnv["DATABASE_URL"]).orElseThrow();
+            mapConfigHibernete[Environment.USER] = Optional.ofNullable(listEnv["DATABASE_USERNAME"]).orElseThrow();
+            mapConfigHibernete[Environment.PASS] = Optional.ofNullable(listEnv["DATABASE_PASSWORD"]).orElseThrow();
+            mapConfigHibernete[Environment.DIALECT] = Optional.ofNullable(listEnv["DIALECT"]).orElseThrow();
+            mapConfigHibernete[Environment.FORMAT_SQL] = "true";
+            mapConfigHibernete[Environment.SHOW_SQL] = "false";
+            mapConfigHibernete[Environment.CURRENT_SESSION_CONTEXT_CLASS] = "thread";
+            mapConfigHibernete[Environment.HBM2DDL_AUTO] = "update";
+            mapConfigHibernete[Environment.POOL_SIZE] = "5";
+            java.util.logging.Logger.getLogger("org.hibernate").level = Level.OFF;
+            return Persistence.createEntityManagerFactory("main",mapConfigHibernete)
+        }
+
+        val entityManager = createEntityManager(System.getenv())
+    }
+}
+
+
